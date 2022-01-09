814e53e78d3ce82d45127738f537ceee2f03869a
yassineoo <61011356+yassineoo@users.noreply.github.com>
2022-01-09 11:21:15 +0100

    db-setup

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const  mongoose =require( "mongoose");
+const  bodyParser  =require( "body-parser");
+const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 
